9d0fb262f1b47cd46065e9d3408a35bca5497f66
Gaurav Waghmare <w.gaurav007@gmail.com>
2021-08-21 11:37:56 +0530

    Deleted files

--- php_coding_standards.sh
+++ php_coding_standards.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-## https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
+
+sudo apt install -y php8.0-xml;
+
 EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
@@ -15,8 +17,9 @@ php composer-setup.php --quiet
 RESULT=$?
 rm composer-setup.php
 
-## reload path
-. ~/.bashrc
+sudo mv composer.phar /usr/local/bin/composer
+
+source ~/.bashrc
 
 composer global require "squizlabs/php_codesniffer=*"
 composer global require dealerdirect/phpcodesniffer-composer-installer;
