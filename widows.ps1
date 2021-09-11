392ceba7f3a6af6621e3f2f7f9ec6dcc04976437
Gaurav Waghmare <57717115+gauravw15@users.noreply.github.com>
2021-09-11 14:16:37 +0530

    Add whatsapp

--- widows.ps1
+++
@@ -1,6 +0,0 @@
-choco install vscode;
-choco install slack;
-choco install googlechrome;
-choco install vlc;
-choco install microsoft-windows-terminal;
-choco install nodejs-lts;
---
+++ windows.ps1
@@ -0,0 +1,7 @@
+choco install vscode -y;
+choco install slack -y;
+choco install googlechrome -y;
+choco install vlc -y;
+choco install microsoft-windows-terminal -y;
+choco install nodejs-lts -y;
+choco install whatsapp -y;
